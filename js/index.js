18463ea143def5a165a2d17b45efd64501a78464
lswhobby <89378405+lswhobby@users.noreply.github.com>
2021-08-24 12:54:17 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,23 @@
 let a = new Vue({
   el:"vues",
   data:{
-  
+    mid:"",
+    mpass:"",
   },
   methods:{
-  
+    gopage:function(){
+      if(this.mid==""){
+          alert("아이디를 입력해주세요");
+          return false;
+      }
+      else if(this.mpass==""){
+        alert("아이디를 입력해주세요");
+          return false;
+      }
+      else{      
+        location.href="http://lswhobby.dothome.co.kr";
+      }
+    }
   }
 
 });
